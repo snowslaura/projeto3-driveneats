eee78e724f77c26e84b087498d0b101bd2e7acad
Laura Neves <lauracmneves@gmail.com>
2022-01-27 22:16:12 -0300

    Inicio do javascript

---
+++ script.js
@@ -0,0 +1,56 @@
+function escolherPratoPesto() {
+    document.getElementById("pesto").style.boxShadow = "0px 0px 10px -4px rgba(0, 0, 0, 0.25), inset 0px 0px 0px 5px #32B72F";
+    document.getElementById("burguer").style.boxShadow = "0px 0px 10px -4px rgba(0, 0, 0, 0.25)";
+    document.getElementById("salada").style.boxShadow = "0px 0px 10px -4px rgba(0, 0, 0, 0.25)";
+   
+}
+
+function escolherPratoBurguer() {
+    document.getElementById("pesto").style.boxShadow = "0px 0px 10px -4px rgba(0, 0, 0, 0.25)";
+    document.getElementById("burguer").style.boxShadow =   "0px 0px 10px -4px rgba(0, 0, 0, 0.25), inset 0px 0px 0px 5px #32B72F"
+    document.getElementById("salada").style.boxShadow = "0px 0px 10px -4px rgba(0, 0, 0, 0.25)";
+}
+
+function escolherPratoSalada() {
+    document.getElementById("pesto").style.boxShadow = "0px 0px 10px -4px rgba(0, 0, 0, 0.25)";
+    document.getElementById("burguer").style.boxShadow = "0px 0px 10px -4px rgba(0, 0, 0, 0.25)";
+    document.getElementById("salada").style.boxShadow = "0px 0px 10px -4px rgba(0, 0, 0, 0.25), inset 0px 0px 0px 5px #32B72F";
+}
+
+function escolherBebidaCoca() {
+    document.getElementById("coca").style.boxShadow = "0px 0px 10px -4px rgba(0, 0, 0, 0.25), inset 0px 0px 0px 5px #32B72F";
+    document.getElementById("suco").style.boxShadow = "0px 0px 10px -4px rgba(0, 0, 0, 0.25)";
+    document.getElementById("chopp").style.boxShadow = "0px 0px 10px -4px rgba(0, 0, 0, 0.25)";
+   
+}
+
+function escolherBebidaSuco(){
+    document.getElementById("coca").style.boxShadow = "0px 0px 10px -4px rgba(0, 0, 0, 0.25)";
+    document.getElementById("suco").style.boxShadow =   "0px 0px 10px -4px rgba(0, 0, 0, 0.25), inset 0px 0px 0px 5px #32B72F";
+    document.getElementById("chopp").style.boxShadow = "0px 0px 10px -4px rgba(0, 0, 0, 0.25)";
+}
+
+function escolherBebidaChopp(){
+    document.getElementById("coca").style.boxShadow = "0px 0px 10px -4px rgba(0, 0, 0, 0.25)";
+    document.getElementById("suco").style.boxShadow = "0px 0px 10px -4px rgba(0, 0, 0, 0.25)";
+    document.getElementById("chopp").style.boxShadow = "0px 0px 10px -4px rgba(0, 0, 0, 0.25), inset 0px 0px 0px 5px #32B72F";
+}
+
+function escolherSobremesaPudim(){
+    document.getElementById("pudim").style.boxShadow = "0px 0px 10px -4px rgba(0, 0, 0, 0.25), inset 0px 0px 0px 5px #32B72F";
+    document.getElementById("torta").style.boxShadow = "0px 0px 10px -4px rgba(0, 0, 0, 0.25)";
+    document.getElementById("cake").style.boxShadow = "0px 0px 10px -4px rgba(0, 0, 0, 0.25)";
+   
+}
+
+function escolherSobremesaTorta(){
+    document.getElementById("pudim").style.boxShadow = "0px 0px 10px -4px rgba(0, 0, 0, 0.25)";
+    document.getElementById("torta").style.boxShadow =   "0px 0px 10px -4px rgba(0, 0, 0, 0.25), inset 0px 0px 0px 5px #32B72F";
+    document.getElementById("cake").style.boxShadow = "0px 0px 10px -4px rgba(0, 0, 0, 0.25)";
+}
+
+function escolherSobremesaCake(){
+    document.getElementById("pudim").style.boxShadow = "0px 0px 10px -4px rgba(0, 0, 0, 0.25)";
+    document.getElementById("torta").style.boxShadow = "0px 0px 10px -4px rgba(0, 0, 0, 0.25)";
+    document.getElementById("cake").style.boxShadow = "0px 0px 10px -4px rgba(0, 0, 0, 0.25), inset 0px 0px 0px 5px #32B72F";
+}
